a4e60845cc90d9fe3c10f042dc9fe83261bd243d
Youness Bourgui <younessbourgui07@gmail.com>
2024-08-27 11:16:27 +0200

    Create the repos

---
+++ src/main/java/main/repo/EvidenceRepo.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package main.repo;
+
+import java.util.List;
+import main.entity.Evidence;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author hp
+ */
+@Repository
+public interface EvidenceRepo extends JpaRepository<Evidence,Integer>{
+    List<Evidence> findByCriminalCaseId(Integer id);
+    List<Evidence> findByStorageId(Integer id);
+    boolean existsByEvidenceNumber(String number);
+    List<Evidence> findByIsArchivedTrue();
+    @Override
+    Page<Evidence> findAll(Pageable pageable);
+}
---
+++ src/main/java/main/repo/TrackEntityRepo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package main.repo;
+
+import main.entity.TrackEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author hp
+ */
+@Repository
+public interface TrackEntityRepo extends JpaRepository<TrackEntity,Integer>{
+    @Override
+    Page<TrackEntity> findAll(Pageable pageable);
+}
